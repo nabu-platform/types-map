2a2a2042865a3b38d98e8a673f71759c9afabca0
Alexander Verbruggen <alex@nabu.be>
2021-12-04 07:41:39 +0100

    updated

--- src/main/java/be/nabu/libs/types/map/MapContent.java
+++ src/main/java/be/nabu/libs/types/map/MapContent.java
@@ -1,10 +1,13 @@
 package be.nabu.libs.types.map;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
 
 import be.nabu.libs.types.CollectionHandlerFactory;
 import be.nabu.libs.types.ComplexContentWrapperFactory;
@@ -14,7 +17,10 @@ import be.nabu.libs.types.api.CollectionHandlerProvider;
 import be.nabu.libs.types.api.ComplexContent;
 import be.nabu.libs.types.api.ComplexType;
 import be.nabu.libs.types.api.DefinedSimpleType;
+import be.nabu.libs.types.api.Element;
+import be.nabu.libs.types.api.ModifiableComplexType;
 import be.nabu.libs.types.api.SimpleTypeWrapper;
+import be.nabu.libs.types.base.ComplexElementImpl;
 import be.nabu.libs.types.base.SimpleElementImpl;
 import be.nabu.libs.types.base.ValueImpl;
 import be.nabu.libs.types.properties.MaxOccursProperty;
@@ -26,6 +32,7 @@ public class MapContent implements ComplexContent {
 	private Map content;
 	private ComplexType type;
 	private boolean literal;
+	private boolean allowNewFields = true;
 
 	public MapContent() {
 		this(new MapType(), new HashMap<String, Object>());
@@ -44,25 +51,33 @@ public class MapContent implements ComplexContent {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public Object get(String path) {
+		Object object;
 		if (content.containsKey(path)) {
-			return content.get(path);
+			object = content.get(path);
 		}
-		ParsedPath parsedPath = new ParsedPath(path);
-		Object object = content.get(parsedPath.getName());
-		if (object != null && parsedPath.getIndex() != null) {
-			CollectionHandlerProvider handler = CollectionHandlerFactory.getInstance().getHandler().getHandler(object.getClass());
-			if (handler == null) {
-				throw new IllegalArgumentException("No collection handler for: " + object);
+		else {
+			ParsedPath parsedPath = new ParsedPath(path);
+			object = content.get(parsedPath.getName());
+			if (object != null && parsedPath.getIndex() != null) {
+				CollectionHandlerProvider handler = CollectionHandlerFactory.getInstance().getHandler().getHandler(object.getClass());
+				if (handler == null) {
+					throw new IllegalArgumentException("No collection handler for: " + object);
+				}
+				object = handler.get(object, handler.unmarshalIndex(parsedPath.getIndex()));
 			}
-			object = handler.get(object, handler.unmarshalIndex(parsedPath.getIndex()));
+			if (object != null && parsedPath.getChildPath() != null) {
+				if (object instanceof ComplexContent) {
+					object = ((ComplexContent) object).get(parsedPath.getChildPath().toString());
+				}
+				else {
+					object = ComplexContentWrapperFactory.getInstance().getWrapper().wrap(object).get(parsedPath.getChildPath().toString());
+				}
+			}
 		}
-		if (object != null && parsedPath.getChildPath() != null) {
-			if (object instanceof ComplexContent) {
-				object = ((ComplexContent) object).get(parsedPath.getChildPath().toString());
-			}
-			else {
-				object = ComplexContentWrapperFactory.getInstance().getWrapper().wrap(object).get(parsedPath.getChildPath().toString());
-			}
+		// this should only ever occur at the deepest level
+		// we don't want the Map to be interpreted as a key/value store (e.g. for xml marshalling)
+		if (object instanceof Map) {
+			return new MapContent((ComplexType) getType().get(path).getType(), (Map) object);
 		}
 		return object;
 	}
@@ -83,19 +98,34 @@ public class MapContent implements ComplexContent {
 	
 			// delegate the setting to another object
 			if (parsedPath.getChildPath() != null) {
-				ParsedPath pathToSet = parsedPath;
+				ParsedPath pathToSet = parsedPath.getChildPath();
+				parsedPath.setChildPath(null);
 				// we need to get the last part of the path
-				while (parsedPath.getChildPath() != null) {
-					if (parsedPath.getChildPath().getChildPath() == null) {
-						pathToSet = parsedPath.getChildPath();
-						parsedPath.setChildPath(null);
-						break;
-					}
-				}
+//				while (parsedPath.getChildPath() != null) {
+//					if (parsedPath.getChildPath().getChildPath() == null) {
+//						pathToSet = parsedPath.getChildPath();
+//						parsedPath.setChildPath(null);
+//						break;
+//					}
+//				}
 				Object targetObject = get(parsedPath.toString());
 				if (targetObject == null) {
-					// TODO: simply add a new map content with a dynamic map type?
-					throw new IllegalStateException("Could not find: " + parsedPath.toString() + " in path: " + path);
+					Element<?> element = getType().get(parsedPath.getName());
+					if (element == null) {
+						if (getType() instanceof ModifiableComplexType && allowNewFields) {
+							MapType mapType = new MapType();
+							element = new ComplexElementImpl(parsedPath.toString(), mapType, getType(), new ValueImpl<Integer>(MinOccursProperty.getInstance(), 0));
+							((ModifiableComplexType) getType()).add(element);
+						}
+						else {
+							throw new IllegalStateException("Could not find: " + parsedPath.toString() + " in path: " + path);
+						}
+					}
+					if (!(element.getType() instanceof ComplexType)) {
+						throw new IllegalStateException("The field is not complex: " + parsedPath.toString() + " in path " + path);
+					}
+					targetObject = ((ComplexType) element.getType()).newInstance();
+					content.put(parsedPath.getName(), targetObject);
 				}
 				if (!(targetObject instanceof ComplexContent)) {
 					targetObject = ComplexContentWrapperFactory.getInstance().getWrapper().wrap(targetObject);
@@ -130,8 +160,15 @@ public class MapContent implements ComplexContent {
 								break;
 							}
 						}
-						if (clazz == null) {
+						// we add the ParameterizedType check, otherwise, if you are using raw lists, the component type thingy will throw an exception
+						if (clazz == null && value.getClass() instanceof Type && ((Type) value.getClass()) instanceof ParameterizedType) {
 							handler.getComponentType(value.getClass());
+						}
+						// currently if we can't determine what is in there, we could either opt for Object.class or String.class
+						// however, because the following code is focused on simple types, we use string atm
+						// in the future we could make this more complex
+						else {
+							clazz = String.class;
 						}
 					}
 					DefinedSimpleType<? extends Object> wrap = SimpleTypeWrapperFactory.getInstance().getWrapper().wrap(clazz);
