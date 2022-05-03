626f3886b59514244343b44e497386f6500e20cc
Alexander Verbruggen <alex@nabu.be>
2022-05-03 19:18:00 +0200

    added wrap mapping but default off

--- src/main/java/be/nabu/libs/types/map/MapContent.java
+++ src/main/java/be/nabu/libs/types/map/MapContent.java
@@ -8,7 +8,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.Callable;
 
 import be.nabu.libs.types.CollectionHandlerFactory;
 import be.nabu.libs.types.ComplexContentWrapperFactory;
@@ -20,7 +19,6 @@ import be.nabu.libs.types.api.ComplexType;
 import be.nabu.libs.types.api.DefinedSimpleType;
 import be.nabu.libs.types.api.Element;
 import be.nabu.libs.types.api.ModifiableComplexType;
-import be.nabu.libs.types.api.SimpleTypeWrapper;
 import be.nabu.libs.types.base.ComplexElementImpl;
 import be.nabu.libs.types.base.SimpleElementImpl;
 import be.nabu.libs.types.base.ValueImpl;
@@ -34,7 +32,8 @@ public class MapContent implements ComplexContent {
 	private ComplexType type;
 	private boolean literal;
 	private boolean allowNewFields = true;
-
+	private boolean wrapMaps = false;
+	
 	public MapContent() {
 		this(new MapType(), new HashMap<String, Object>());
 	}
@@ -75,43 +74,68 @@ public class MapContent implements ComplexContent {
 				}
 			}
 		}
-		// this should only ever occur at the deepest level
-		// we don't want the Map to be interpreted as a key/value store (e.g. for xml marshalling)
-		if (object instanceof Map) {
-			// we _do_ have complex keys at which point we should hit the first branch
-			// we could verify this further, but for now we leave it like this
-			Element<?> element = getType().get(path);
-			if (element == null) {
-				element = getType().get(parsedPath.getName());
-				if (element == null) {
-					throw new RuntimeException("Can not find path: " + path);
-				}
-			}
-			return new MapContent((ComplexType) element.getType(), (Map) object);
+		
+		// in glue maps are handled as flat key/value pairs
+		// that means if we want to do object-access (e.g. we do a set("configuration/properties", something), it actually uses the full path as the key, rather than nesting maps
+		// this means, if we xml.objectify() something and we get plain maps back, object access might malfunction, which is what happened during deployment where "configuration/properties" was seen as a single key in a statement "part/configuration/properties = keyValuePairs"
+		// this in turn was not correctly marshalled back into an XML and it was quietly ignored
+		// there was in fact an object "configuration" which already had a subobject "properties", but the flat map handling does not recognize this
+		// for those cases, it is better to autowrap maps into mapcontent, which enables complex handling
+		
+		// however, some other modules (notably the swagger provider) builds correct hierarchical maps but does not do well when the typing is enforced through mapcontent
+		// instead it works with maps and presumably does a MapContentWrapper wrapping which dynamically generates a new type based on the map rather than using the centralized type available in map content
+		// because swagger is so fked up definition wise, there is no single type that can actually describe it
+		// however, specifically because we auto-wrap maps and _don't_ want the behavior there, it is disabled by default
+		// the only real known problem with maps is when path-like keys are not enforced to be hierarchic
+		if (!wrapMaps) {
+			return object;
 		}
-		// we might have an iteration of maps
-		else if (object instanceof Iterable) {
-			List result = new ArrayList();
-			for (Object single : (Iterable) object) {
-				if (single instanceof Map) {
-					Element<?> element = getType().get(path);
-					if (element == null) {
-						element = getType().get(parsedPath.getName());
-						if (element == null) {
-							throw new RuntimeException("Can not find path: " + path);
-						}
-					}
-					result.add(new MapContent((ComplexType) element.getType(), (Map) single));
-				}
-				else {
-					result.add(single);
-				}
+		// we _do_ have complex keys at which point we should hit the first branch
+		// we could verify this further, but for now we leave it like this
+		Element<?> element = getType().get(path);
+		if (element == null) {
+			element = getType().get(parsedPath.getName());
+		}
+		if (element != null && element.getType() instanceof ComplexType) {
+			// this should only ever occur at the deepest level
+			// we don't want the Map to be interpreted as a key/value store (e.g. for xml marshalling)
+			if (object instanceof Map) {
+				// we'll assume (for now) that the accompanying type is correct and does not need local enriching, at least not in the get(), it should already be automatically enriched during the set
+//					object = copy((ComplexType) element.getType(), (Map) object);
+				object = new MapContent((ComplexType) element.getType(), (Map) object);
+				// inherit the wrap maps so child content also enforces this
+				((MapContent) object).wrapMaps = true;
 			}
-			object = result;
+			// we might have an iteration of maps
+			else if (object instanceof Iterable && element.getType().isList(element.getProperties())) {
+				List result = new ArrayList();
+				for (Object single : (Iterable) object) {
+					if (single instanceof Map) {
+						single = new MapContent((ComplexType) element.getType(), (Map) single);
+						((MapContent) single).wrapMaps = true;
+						result.add(single);
+//							result.add(copy((ComplexType) element.getType(), (Map) single));
+					}
+					else {
+						result.add(single);
+					}
+				}
+				object = result;
+			}
 		}
 		return object;
 	}
-
+	// this basically forces the complextype (assuming its a maptype) to be updated because the mapcontent will update the maptype as it sees new values
+	// so basically its a lazy reuse of that logic
+	private MapContent copy(ComplexType type, Map map) {
+		MapContent result = new MapContent(type, new HashMap());
+		Set<Map.Entry<String, Object>> entrySet = map.entrySet();
+		for (Map.Entry<String, Object> entry : entrySet) {
+			result.set(entry.getKey(), entry.getValue());
+		}
+		return new MapContent(type, map);
+	}
+	
 	@Override
 	public ComplexType getType() {
 		return type;
@@ -207,6 +231,7 @@ public class MapContent implements ComplexContent {
 					}
 					else {
 						// TODO: check for complex types?
+						System.out.println("-----------------_> hitting it!");
 					}
 				}
 			}
@@ -236,4 +261,13 @@ public class MapContent implements ComplexContent {
 		}
 		return map;
 	}
+
+	public boolean isWrapMaps() {
+		return wrapMaps;
+	}
+
+	public void setWrapMaps(boolean wrapMaps) {
+		this.wrapMaps = wrapMaps;
+	}
+	
 }
